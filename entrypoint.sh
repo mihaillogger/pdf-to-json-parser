7189c553d6fbe82510d7e0bf390e83ac75251117
Roman Kornyakov <romankorniakov4833@gmail.com>
2026-06-09 15:41:34 +0300

    fix: fix mypy and docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,7 @@ echo "2. Проверка типов (Mypy)"
 uv run mypy
 
 echo "3. Запуск готовых тестов"
-uv run pytest tests/test_metadata.py tests/test_sections.py -v --cov=src --cov-report=term-missing
-uv run pytest tests/test_integration.py -v --cov=src --cov-report=term-missing
-# после того как все напишут тесты заменить на:
-# uv run pytest tests/ -v --cov=src --cov-report=term-missing
+uv run pytest tests/ -v --cov=src --cov-report=term-missing
 
 echo "Проверки пройдены!"
 exec "$@"
